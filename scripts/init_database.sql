86ff79c4a691108733b003c7ea1728355cfa8170
ML15028 <128655063+ML15028@users.noreply.github.com>
2026-03-04 09:41:14 -0600

    Create init_database.sql

---
+++ scripts/init_database.sql
@@ -0,0 +1,18 @@
+/*
+
+Script propose:
+
+Create database and schemas such as Silver, Gold, Bronze
+
+*/
+
+CREATE DATABASE DataWarehouse;
+
+use DataWarehouse;
+
+CREATE SCHEMA bronze;
+go
+CREATE SCHEMA silver;
+go
+CREATE SCHEMA gold;
+go
