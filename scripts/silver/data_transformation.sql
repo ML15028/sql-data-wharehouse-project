6eeedc84e388056e6210feeff57478d7f35ed53f
ML15028 <128655063+ML15028@users.noreply.github.com>
2026-04-23 15:40:03 -0600

    data_transformation.sql

--- scripts/silver/data_transformation.sql
+++ scripts/silver/data_transformation.sql
@@ -106,6 +106,29 @@ SELECT
         else [sls_price]
         end AS [sls_price]
   FROM [bronze].[crm_sales_details]
+
+-- CUSTOMER AZ12 TABLE
+
+
+INSERT INTO silver.erp_cust_az12(
+cid,
+bdate,
+gen
+)
+
+
+SELECT  case when [cid] like 'NAS%' then SUBSTRING (cid,4,len(cid))
+        else cid 
+       end as cid
+      ,case when bdate >getdate() then null
+      else bdate
+      end as bdate
+      ,case when  upper(trim(gen)) in ('F','FEMALE') THEN 'Female'
+            when  upper(trim(gen)) in ('M','MALE') THEN 'Male'
+        else 'n/a'
+        end as gen
+  FROM [DataWarehouse].[bronze].[erp_cust_az12]
+  
   
 
 
